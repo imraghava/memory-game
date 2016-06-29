bf625129fc98a4e10b09274f37ce04b1bc688832
Michelle Levine <michellejl@live.com>
2016-06-28 19:07:34 -0700

    starting to build out game logic

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+var up = 0;
+var remaining = 8;
+var found = 0;
+var checked = 0;
+
+// When a card is clicked
 $('.card').on('click', function() {
-  $(this).addClass('flipped');
+
   if ($(this).hasClass('a')) {
     $(this).text('A');
   }
+  
+   
+  
+  if ($('.flipped').hasClass('a') && $(this).hasClass('a')) {
+    $(this).addClass('flipped');
+    alert("Match!");
+    $('.flipped').remove();
+  } else {
+    $(this).addClass('flipped');
+  }
+  
+  
+  
+ 
+  
+  
+ 
+  
+  up++;
+  if (up > 2) { 
+    up = 0;
+    $('.flipped').removeClass('flipped');
+  } else {
+     checked++;
+    $('.checked').text(checked);
+  }
+  
+  
+  
+  
 });

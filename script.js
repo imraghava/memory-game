a42958406f560cea24c761622b7d969561f7762b
Michelle Levine <michellejl@live.com>
2016-07-01 15:57:51 -0700

    trying to make promise like things work

--- script.js
+++ script.js
@@ -2,35 +2,52 @@
 var num;
 var url_base = 'http://pokeapi.co/api/v2/pokemon/';
 var url;
-var cards = [];
+var card;
+var deck = [];
 
 
 
-function getCards() {
-  // Generate a list of 10 numbers between 1 and 150
+function makeDeck() {
+  
   for (var i = 0; i < 10; i++) {
+    // Generate a list of 10 numbers between 1 and 150 and create URLs with them
     num = Math.floor(Math.random() * 150) + 1;
-
     url = url_base + num + '/';
-
-    // API call to Pokemon API
-    $.getJSON(url, function(data) {
-      var pkmnName = data.name;
-      var pkmnSprite = data.sprites.front_default;
-      var pkmnID = data.id;
-
-      var card = {
-          name: pkmnName,
-          img: pkmnSprite,
-          id: pkmnID
-        };
-
-      cards.push(card);
-    });
-
+    
+    
+    makeCard();
+    
+    
+    
   } // end for
+  
 } // end function
 
 
 
+function makeCard() {
+  // API call to Pokemon API
+  
+  $.getJSON(url, function(data) {
+    // Pull out the three items I need
+    var pkmnName = data.name;
+    var pkmnSprite = data.sprites.front_default;
+    var pkmnID = data.id;
 
+    // Create a card object
+    card = {
+        name: pkmnName,
+        img: pkmnSprite,
+        id: pkmnID
+      };
+    
+    console.log(card);
+    deck.push(card);
+    return(deck);
+  });
+  
+  
+  
+}
+
+makeDeck();

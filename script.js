e27f87536823be8212915eeb543568649ba4e12b
Michelle Levine <michellejl@live.com>
2016-06-30 23:37:21 -0700

    starting to rework code

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-var up = 0;
-var remaining = 8;
-var found = 0;
-var checked = 0;
+// Tracker Variables
+var up;
+var remaining;
+var found;
+var checked;
+
+// Create a function that creates the cards
+var Card = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.width = 70;
+};
+
+Card.prototype.drawFaceDown = function() {
+  fill(214, 247,202);
+  strokeWeight(2);
+  rect(this.x, this.y, this.width, this.width, 10);
+  // image(getImage("images/back"), this.x, this.y, this.width, this.width);
+};
+
+
+
+var cards = [];
+
+var COLS = 5;
+var ROWS = 4;
+
+
+for (var i = 0; i < COLS; i++) {
+  for (var j = 0; j < ROWS; j++) {
+    cards.push(new CARD( i * 78 + 10, j * 78 + 40))
+  }
+}
+
+for (var i = 0; i < tiles.length; i++) {
+    tiles[i].drawFaceDown();
+}
 
 // When a card is clicked
 $('.card').on('click', function() {
 
 
-  
-  
-  
-  
 });
-
-// Issues that need to be solved:
-// 1 - clicking on the same card twice produces a "match"
-// 2 - need a better way to add content to cards... I don't want to have 8 if statements if there is a more efficient way to do it

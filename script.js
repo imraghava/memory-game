9d1eb66c1c06186c0f351f0ec899d0e2715c1cc8
Michelle Levine <michellejl@live.com>
2016-07-01 20:08:05 -0700

    got my API call functioning in a way that works for me. Built deck

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
 // var numbers = [];
 var num;
-var url_base = 'http://pokeapi.co/api/v2/pokemon/';
+var url = 'http://pokeapi.co/api/v2/pokemon/?limit=150';
 var url;
 var card;
 var deck = [];
 
 
-
-function makeDeck() {
+// API call to Pokemon API
+$.getJSON(url, function(data) {
+  
   
   for (var i = 0; i < 10; i++) {
     // Generate a list of 10 numbers between 1 and 150 and create URLs with them
     num = Math.floor(Math.random() * 150) + 1;
-    url = url_base + num + '/';
     
-    
-    makeCard();
-    
-    
-    
+    makeCard(num, data);
+     
   } // end for
   
-} // end function
-
-
-
-function makeCard() {
-  // API call to Pokemon API
+  console.log(deck);
   
-  $.getJSON(url, function(data) {
-    // Pull out the three items I need
-    var pkmnName = data.name;
-    var pkmnSprite = data.sprites.front_default;
-    var pkmnID = data.id;
+});
 
-    // Create a card object
-    card = {
-        name: pkmnName,
-        img: pkmnSprite,
-        id: pkmnID
-      };
-    
-    console.log(card);
-    deck.push(card);
-    return(deck);
-  });
+function makeCard(num, data) {
+  var info = data.results[num];
   
+  card = {
+    'pkmnName' : info.name,
+    'pkmnSprite' : 'http://pokeapi.co/media/sprites/pokemon/' + num + '.png',
+    'pkmnID' : num
+  };
   
-  
-}
+  deck.push(card);
+};
 
-makeDeck();

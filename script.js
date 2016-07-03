9851216dabd8fd8d888c074d8c4c915ab114da5f
Michelle Levine <michellejl@live.com>
2016-07-02 21:54:22 -0700

    creating cards with images and pokemon names

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var deck = [];
 // Function to make a card object
 function makeCard(num, data) {
   var info = data.results[num];
+  num = num + 1;
   
   card = {
     'pkmnName' : info.name,
@@ -15,9 +16,48 @@ function makeCard(num, data) {
     'pkmnID' : num
   };
   
-  // Add new card to deck
+  // Add new card to deck twice so that each card has a match
+  deck.push(card);
   deck.push(card);
 };
+
+
+// Randomizing order of cards using code from: https://bost.ocks.org/mike/shuffle/
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+};
+
+
+// Build out the gameboard
+function makeGameBoard(array) {
+  for (var j = 0; j < 20; j++) {
+    var placedCard = array.pop();
+    var id = placedCard.pkmnID + 1;
+    
+    var cardString = '<div class="card ' + id + '">' + placedCard.pkmnName + '<img src="' + placedCard.pkmnSprite + '" /></div>';
+    
+    $("#gameboard").append(cardString);
+    
+
+  }
+  
+  
+};
+
 
 
 // API call to Pokemon API
@@ -31,8 +71,12 @@ $.getJSON(url, function(data) {
     makeCard(num, data);  
   } // end for
   
-  console.log(deck);
+  // Randomize the order of the deck
+  var shuffled = shuffle(deck);
+  
+  // Make HTML
+  makeGameBoard(shuffled);
+  console.log(shuffled);
   
 });
-
 
